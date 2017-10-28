915656e95278d155fa932a3d8072916d4de32362
tanzby <tanzby@outlook.com>
2017-10-28 18:13:32 +0800

    lab4
    notification

---
+++ lab4/codes/app/src/main/java/pub/tanzby/lab4/ProductDetailActivity.java
@@ -0,0 +1,120 @@
+package pub.tanzby.lab4;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProductDetailActivity extends AppCompatActivity {
+    @BindView(R.id.favoriteBnt)        ImageButton Fav;
+    @BindView(R.id.addShoppingListBnt) ImageButton Add;
+    @BindView(R.id.returnBnt)          ImageButton Ret;
+    @BindView(R.id.productName)        TextView    Name;
+    @BindView(R.id.productWeight)      TextView    PrioInfo;
+    @BindView(R.id.productPrice)       TextView    Price;
+    @BindView(R.id.productView)        ImageView   proImg;
+    @BindView(R.id.moreOption)         ListView    moreOption;
+    private Product curProduct;
+    private Integer curProductPos;
+    private Boolean is_Favor;
+    private Integer PurchaseNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+        init();
+        getDataFromIntent();
+        setEven();
+    }
+
+    private void init()
+    {
+        moreOption.setAdapter(
+                new ArrayAdapter<String>(
+                        ProductDetailActivity.this,
+                        R.layout.more_opt_item_view,
+                        new String[]{"一键下单","分享商品","不感兴趣","查看更多商品促销信息"}
+                ));
+    }
+
+    private void getDataFromIntent() {
+        curProduct = (Product)  this.getIntent().getParcelableExtra("PRODUCT");
+        curProductPos = this.getIntent().getIntExtra("PRODUCT_Position",-1);
+        Name.setText(curProduct.getName());
+        proImg.setImageResource(curProduct.getImgObject());
+        PrioInfo.setText(curProduct.getSpecialInfo());
+        Price.setText(curProduct.getPrice());
+        is_Favor = curProduct.getIs_favorite();
+        PurchaseNum   = curProduct.getPurchaseNumber();
+
+        setOptionBnt(Fav,is_Favor,
+                R.drawable.ic_star_pink_a400_48dp,
+                R.drawable.ic_star_border_grey_500_48dp);
+        Add.setBackgroundResource(R.drawable.ic_add_shopping_cart_black_48dp);
+    }
+
+    private void setOptionBnt(ImageButton bnt, Boolean value, int trueOpt, int falseOpt)
+    {
+        if (value) bnt.setBackgroundResource(trueOpt);
+        else bnt.setBackgroundResource(falseOpt);
+    }
+
+    private void setEven()
+    {
+        Ret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new productMessageEvent(curProduct,curProductPos));
+                finish();
+            }
+        });
+        Fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curProduct.setIs_favorite(!curProduct.getIs_favorite());
+                setOptionBnt(Fav,is_Favor,
+                   R.drawable.ic_star_pink_a400_48dp,
+                   R.drawable.ic_star_border_grey_500_48dp);
+                EventBus.getDefault().post(new productMessageEvent(curProduct,curProductPos));
+            }
+        });
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if(curProduct.getPurchaseNumber()==0) {
+                Toast.makeText(ProductDetailActivity.this,
+                        "商品已添加到购物车",Toast.LENGTH_SHORT).show();
+
+                Intent BroadcastIntent =  new Intent("DynamicFilter");
+                BroadcastIntent.putExtra("PRODUCT",curProduct);
+                BroadcastIntent.putExtra("PRODUCT_Position",curProductPos);
+                sendBroadcast(BroadcastIntent);
+            }
+            else
+            {
+                Toast.makeText(ProductDetailActivity.this,
+                        "商品已添加"+(curProduct.getPurchaseNumber()+1)+"件到购物车",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+            curProduct.setPurchaseNumber(curProduct.getPurchaseNumber()+1);
+            EventBus.getDefault().post(new productMessageEvent(curProduct,curProductPos));
+            }
+        });
+    }
+}
---
+++ lab4/codes/app/src/main/java/pub/tanzby/lab4/RCAdapter.java
@@ -0,0 +1,91 @@
+package pub.tanzby.lab4;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+/**
+ * Created by tanzb on 2017/10/20 0020.
+ */
+
+    public class RCAdapter extends RecyclerView.Adapter<RCAdapter.VH> {
+
+    private Context mContext;
+    private List<Product> itemList;
+
+    public void removeData(int position) {
+        itemList.remove(position);
+        notifyItemRemoved(position);
+}
+public Product getItems(int position)
+{
+    return itemList == null?null:itemList.get(position);
+}
+@Override
+public VH onCreateViewHolder(ViewGroup parent, int viewType)
+{
+    VH h = new VH(LayoutInflater
+            .from(mContext)
+            .inflate(R.layout.items_view_for_rc, parent,false));
+    return h;
+}
+
+    @Override
+    public void onBindViewHolder(final VH holder, int position) {
+        holder.fullProductName.setText( itemList.get(position).getName() );
+        holder.firstCharOfProductName.setText(
+                itemList.get(position).getName().substring(0,1).toUpperCase());
+        if (mOnItemClickLitener != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemClick(holder.itemView, pos);
+            }});
+
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemLongClick(holder.itemView, pos);
+                    return false;
+            }});
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return itemList == null?0:itemList.size();
+    }
+
+    public static class VH extends RecyclerView.ViewHolder {
+        TextView fullProductName;
+        TextView firstCharOfProductName;
+        public VH(View itemView) {
+            super(itemView);
+            firstCharOfProductName= (TextView)itemView.findViewById(R.id.id_ItemFirstChar);
+            fullProductName = (TextView)itemView.findViewById(R.id.id_ItemFullName);
+        }
+    }
+
+    public RCAdapter(Context context,List<Product> itemList_) {
+        itemList = itemList_;
+        mContext = context;
+    }
+
+    /*创建Item的点击接口*/
+    public interface OnItemClickLitener
+    {
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view , int position);
+    }
+    private OnItemClickLitener mOnItemClickLitener;
+
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
+    {
+        this.mOnItemClickLitener = mOnItemClickLitener;
+    }
+}
